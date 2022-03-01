48538f42ed78a3663c316cdf4dddb45bbf5a1c8d
Mathieu-R <mathieu0709@gmail.com>
2022-03-01 16:00:12 +0100

    update

--- src/datastructures/LinkedList/LinkedList.java
+++ src/datastructures/LinkedList/LinkedList.java
@@ -1,8 +1,8 @@
 package datastructures.LinkedList;
 
 public interface LinkedList<E> {
-  public boolean empty();
+  public boolean empty(Node currentNode);
   public void push(E item);
-  public void pop(E item) throws  EmptyLinkedListException;
-  public void deleteWithValue(E item);
+  public void pop() throws EmptyLinkedListException;
+  public void delete(E item);
 }
--- src/datastructures/LinkedList/MyLinkedList.java
+++ src/datastructures/LinkedList/MyLinkedList.java
@@ -1,15 +1,23 @@
 package datastructures.LinkedList;
 
+import java.util.NoSuchElementException;
+
 public class MyLinkedList<E> implements LinkedList<E> {
-  private Node<E> head; // head of the linkedlist
+  private Node head; // head of the linkedlist (first Node)
+  private Node tail; // tail of the linkedlist (last Node)
+  private int size; // size of the linkedlist
 
   private class Node {
     private E item;
-    private Node<E> next;
+    private Node next;
 
-    public Node<E>(E element, Node<E> next) {
-      this.item = element;
-      this.next = next;
+    public Node(E element) {
+      item = element;
+    }
+
+    public Node(E element, Node next) {
+      item = element;
+      next = next;
     }
   }
 
@@ -18,45 +26,71 @@ public class MyLinkedList<E> implements LinkedList<E> {
     return (currentNode.next == null);
   }
 
-  @Override
-  public void append(int data) {
-    if (head == null) {
-      head = new Node(data);
-      return;
+  public E get(int index) throws NoSuchElementException {
+    if ((index > size - 1) || (index < 0)) {
+      throw new NoSuchElementException();
     }
 
-    Node currentNode = head;
-    while (!empty(currentNode)) {
-      currentNode = currentNode.next;
+
+    // we begin at the first element (head)
+    Node pointingNode = head;
+    for (int i = 0; i < index; i++) {
+      pointingNode = pointingNode.next;
     }
 
-    currentNode.next = new Node(data);
+    return pointingNode.item;
   }
 
   @Override
-  public void prepend (int data) {
-    Node newHead = new Node(data);
-    newHead.next = head;
-    head = newHead;
+  public void push(E item) {
+    Node node = new Node(item);
+
+    if (head == null) {
+      head = node;
+      tail = node;
+    } else {
+      tail.next = node;
+      tail = tail.next;
+    }
+
+    size++;
   }
 
   @Override
-  public void deleteWithValue(int data) {
+  public void pop() throws EmptyLinkedListException {
+    if (head == null) {
+      throw new EmptyLinkedListException();
+    }
+
+    Node pointingNode = head;
+    int i = 0;
+    while(i < size - 1) {
+      pointingNode = pointingNode.next;
+      i++;
+    }
+
+    tail = pointingNode;
+    size -= 1;
+  }
+
+  @Override
+  public void delete(E item) {
     if (head == null) {
       return;
     }
 
-    if (head.data == data) {
+    if (head.item == item) {
       head = head.next;
     }
 
     Node currentNode = head;
     while(currentNode.next != null) {
-      if (currentNode.data == data) {
+      if (currentNode.item == item) {
         currentNode.next = currentNode.next.next;
         return;
       }
       currentNode = currentNode.next;
+      size--;
     }
   }
 }
